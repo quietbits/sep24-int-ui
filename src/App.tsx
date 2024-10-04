5f4465390ad5a54388cc3be8ea0eced7d83f4470
Iveta <quietbits@gmail.com>
2024-10-04 13:34:35 -0400

    Set tx data

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,8 @@ const ASSET_ICON: { [key: string]: string } = {
   USDC: assetUsdcIconSrc,
 };
 
-const SEP24_SERVER_URL = "https://anchor-sep-server-m24.stellar.org/sep24";
-
-// https://anchor-sep-server-m24.stellar.org/sep24/transaction?id=bc9e319f-87bb-4a02-8e33-1cb54dcb96a5&lang=en
-
-// https://anchor-reference-server-m24.stellar.org/sep24/interactive?transaction_id=bc9e319f-87bb-4a02-8e33-1cb54dcb96a5&token=eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJiYzllMzE5Zi04N2JiLTRhMDItOGUzMy0xY2I1NGRjYjk2YTUiLCJleHAiOjE3MjgwMDI5MjQsInN1YiI6IkNBSE01TjRYNlJDRFQ1R1BQNFdCSFdDN0xJSERTMktVTURFNFU2NkxLUFpGM1BGUTJMRFU3RU5IIiwiZGF0YSI6eyJhbW91bnQiOiIxIiwibGFuZyI6ImVuIiwiYXNzZXQiOiJzdGVsbGFyOlVTREM6R0RRT0UyM0NGU1VNU1ZRSzRZNUpIUFBZSzczVllDTkhaSEE3RU5LQ1YzN1A2U1VFTzZYUUJLUFAifX0.vFAD33opHaFtuActzLSbxmzsJ9DuOOlPVEl8Vp1BGgg
-
-// ?transaction_id=96248a6d-9409-44d5-b8ae-9e223a3c684f&token=eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5NjI0OGE2ZC05NDA5LTQ0ZDUtYjhhZS05ZTIyM2EzYzY4NGYiLCJleHAiOjE3Mjc5OTk1MjAsInN1YiI6IkNBSE01TjRYNlJDRFQ1R1BQNFdCSFdDN0xJSERTMktVTURFNFU2NkxLUFpGM1BGUTJMRFU3RU5IIiwiZGF0YSI6eyJhbW91bnQiOiIxIiwibGFuZyI6ImVuIiwiYXNzZXQiOiJzdGVsbGFyOlVTREM6R0RRT0UyM0NGU1VNU1ZRSzRZNUpIUFBZSzczVllDTkhaSEE3RU5LQ1YzN1A2U1VFTzZYUUJLUFAifX0.sWSANzqDf3Ox6jc_4ra0n43ELwojkMxg9WxKKqL6bKU
+const REF_SERVER_URL = "https://anchor-reference-server-m24.stellar.org";
+// const REF_SERVER_URL = "http://localhost:8091";
 
 const truncateStr = (str: string, size: number = 4): string => {
   if (2 * size >= str.length) {
@@ -35,44 +30,35 @@ function App() {
 
   const getAssetCode = (asset: string) => asset.split(":")[1];
 
-  // TODO: update these
-  // transaction.to
-  const [txAddress] = useState("CAHM5N4X6RCDT5GPP4WBHWC7LIHDS2KUMDE4U66LKPZF3PFQ2LDU7ENH");
-  // .amount_in
-  const [amountIn] = useState("1");
-  // .amount_out
-  const [amountOut] = useState("0.9");
-  // .amount_out_asset (stellar:USDC:GDQOE23CFSUMSVQK4Y5JHPPYK73VYCNHZHA7ENKCV37P6SUEO6XQBKPP)
-  const [amountOutAsset] = useState(
-    getAssetCode("stellar:USDC:GDQOE23CFSUMSVQK4Y5JHPPYK73VYCNHZHA7ENKCV37P6SUEO6XQBKPP"),
-  );
+  const [txAddress, setTxAddress] = useState("");
+  const [amountIn, setAmountIn] = useState("0");
+  const [amountOut, setAmountOut] = useState("0");
+  const [amountOutAsset, setAmountOutAsset] = useState("");
 
   useEffect(() => {
-    console.log(">>> window.location.search: ", window.location.search);
-
     const searchParams = new URLSearchParams(window.location.search);
-
     const transactionId = searchParams.get("transaction_id");
-    const token = searchParams.get("token");
 
-    console.log(">>> transactionId: ", transactionId);
-    console.log(">>> token: ", token);
-
-    if (!(transactionId && token)) {
+    if (!transactionId) {
       return;
     }
 
     try {
       const fetchTx = async () => {
-        const response = await fetch(`${SEP24_SERVER_URL}/transaction?id=${transactionId}&lang=en`, {
-          headers: { Authorization: `Bearer ${token}` },
+        const response = await fetch(`${REF_SERVER_URL}/transaction`, {
+          headers: { Authorization: `Bearer ${transactionId}` },
         });
 
         return await response.json();
       };
 
       fetchTx().then((res) => {
-        console.log(">>> res: ", res);
+        if (res.destination_account && res.amount_in.amount && res.amount_out.amount && res.amount_out.asset) {
+          setTxAddress(res.destination_account);
+          setAmountIn(res.amount_in.amount);
+          setAmountOut(res.amount_out.amount);
+          setAmountOutAsset(getAssetCode(res.amount_out.asset));
+        }
       });
     } catch (e) {
       console.log(">>> Failed to fetch transaction data: ", e);
@@ -133,11 +119,13 @@ function App() {
     return (
       <>
         <Box gap="md" align="center">
-          <Text size="sm" as="div" addlClassName="App__message">
-            {`Bank transfer was successfully received, sending the above amount to your wallet ${truncateStr(
-              txAddress,
-            )}`}
-          </Text>
+          {txAddress ? (
+            <Text size="sm" as="div" addlClassName="App__message">
+              {`Bank transfer was successfully received, sending the above amount to your wallet ${truncateStr(
+                txAddress,
+              )}`}
+            </Text>
+          ) : null}
         </Box>
 
         <Box gap="lg" justify="end" addlClassName="App__footer">
@@ -165,9 +153,13 @@ function App() {
             Anchor LLC
           </Display>
 
-          <Text size="sm" as="div">
-            {`${truncateStr(txAddress)}`}
-          </Text>
+          <>
+            {txAddress ? (
+              <Text size="sm" as="div">
+                {`${truncateStr(txAddress)}`}
+              </Text>
+            ) : null}
+          </>
         </Box>
       </Box>
 
